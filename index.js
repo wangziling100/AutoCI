11f4b4c94233ef261545ebf9cf8ba1aacfb2112e
wangziling100 <wangziling1000@gmail.com>
2020-08-10 17:39:03 +0200

    Initial commit

---
+++ index.js
@@ -0,0 +1,13 @@
+//const core = require('@actions/core');
+//const wait = require('./wait');
+
+
+// most @actions toolkit packages have async methods
+async function run() {
+  
+}
+
+run();
+module.exports = {
+  run: run,
+}
